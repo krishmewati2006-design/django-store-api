f7b570059f3dfe9eef45c61ee1747c6ae48940b5
krish_mewati <krishmewati2006@gmail.com>
2026-05-16 14:10:12 +0000

    Made Changes In Settings.py

--- store/settings.py
+++ store/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
-
+import dj_database_url
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -77,14 +77,12 @@ WSGI_APPLICATION = 'store.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASSWORD'),
-        "HOST": os.getenv('DB_HOST'),
-    }
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
